0712e3042c0c5f779c80ddc161344c6f8e9486f7
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2023-11-10 01:23:13 +0530

    A1-Q1,Q2 added

---
+++ Assignment01/Q01_WelcomeMSg.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class Q1_WelcomeMSg {
+
+	public static void main(String[] args) {
+		System.out.println("Hello \"ITERIAN\"");
+		System.out.println("Welcome to Siksha ‘O’ Anusandhan Family");
+		System.out.println("Welcome to \"Introduction to Computer Laboratory\"");
+		System.out.println("Java is fun for All !!");
+		
+	}
+
+}
---
+++ Assignment01/Q02_PrintPattern.java
@@ -0,0 +1,9 @@
+package assignment1;
+
+public class Q2_PrintPattern {
+public static void main(String[]args) {
+	System.out.println("*******     *******");
+	System.out.println("      *     *     *");
+	System.out.println("      *******     *******");
+}
+}
