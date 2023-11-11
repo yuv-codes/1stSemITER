2f224108b3c41ec84446c68f74e5587be3327142
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2023-11-11 17:33:05 +0530

    Added A1 - Q3, Q4

---
+++ Assignment01/Q03_PrintPatterm2.java
@@ -0,0 +1,14 @@
+package assignment1;
+
+public class Q03_PrintPatterm2 {
+
+	public static void main(String[] args) {
+	System.out.println("# # # # # # # # # # ");
+	System.out.println("# #             # # ");
+	System.out.println("# #             # # ");
+	System.out.println("# #             # # ");
+	System.out.println("# #             # # ");
+	System.out.println("# # # # # # # # # # ");
+	}
+
+}
---
+++ Assignment01/Q04_PrintName.java
@@ -0,0 +1,7 @@
+package assignment1;
+
+public class Q04_PrintName {
+public static void main(String[]args) {
+	System.out.print("Yuvraj\nSingh" );
+}
+}
