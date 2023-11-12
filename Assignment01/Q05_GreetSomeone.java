1e898168e31b5c6edf2f10d70630344553dd553e
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2023-11-12 22:12:41 +0530

    Added A1 - Q5, Q6

---
+++ Assignment01/Q05_GreetSomeone.java
@@ -0,0 +1,11 @@
+package assignment1;
+
+public class Q05_GreetSomeone {
+
+	public static void main(String[] args) {
+		String A = "Hey Yuvraj!, ";
+		String B = "Good Morning";
+		System.out.print(A+B);
+	}
+
+}
---
+++ Assignment01/Q06_PrintBankInfo.java
@@ -0,0 +1,14 @@
+package assignment1;
+
+public class Q06_PrintBankInfo {
+
+	public static void main(String[] args) {
+		int Acc = 1234567;
+		String name = "Yuvraj";
+		float Bal = 234.00f;
+		System.out.println("My name is " + name +" bearing account number " + Acc + " having balance " + Bal);
+		
+
+	}
+
+}
