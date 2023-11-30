486637df314a475af4c9785a51223156399ba6e0
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2023-12-01 00:59:49 +0530

    Added HomeA- Q01

---
+++ Assignment01/HomeA_Q01_PrintOutput01.java
@@ -0,0 +1,15 @@
+package assignment1;
+
+public class HomeA_Q01_PrintOutput01 {
+
+	public static void main(String[] args) {
+//		What do each of the following print?
+		System.out.println(2 + "bc");
+		System.out.println(2 + 3 + "bc");
+		System.out.println((2+3) + "bc"); 
+		System.out.println("bc" + (2+3));
+		System.out.println("bc" + 2 + 3);
+
+	}
+
+}
