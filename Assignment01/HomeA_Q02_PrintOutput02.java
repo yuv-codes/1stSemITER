b9f13ec0f28938982f0564e773fdfe8ba180396d
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2023-12-02 22:43:14 +0530

    Added HomeA- Q02

---
+++ Assignment01/HomeA_Q02_PrintOutput02.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+public class HomeA_Q02_PrintOutput02 {
+
+	public static void main(String[] args) {
+		System.out.println('b');
+		System.out.println('b' + 'c');
+		System.out.println((char) ('a' + 4));
+
+	}
+
+}
