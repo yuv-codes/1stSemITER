aaf6cdb81757a16be21b75d45a41ec9276f06998
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2023-12-04 23:47:08 +0530

    Added HomeA- Q04

---
+++ Assignment01/HomeA_Q04_PrintOutput04.java
@@ -0,0 +1,16 @@
+package assignment1;
+
+public class HomeA_Q04_PrintOutput04 {
+
+	public static void main(String[] args) {
+		double a = 3.14159;
+		System.out.println(a);
+		System.out.println(a+1);
+		System.out.println(8/(int) a);
+		System.out.println(8/a);
+		System.out.println((int) (8/a));
+
+
+	}
+
+}
