8fcbb4419e3041abc1334b6878f273e2fa875e01
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2023-12-11 21:56:43 +0530

    Added CommandLine - Q03

---
+++ Assignment02/cmd_03.java
@@ -0,0 +1,23 @@
+package assignment1;
+
+public class cmd_03 {
+
+	public static void main(String[] args) {
+		    int a = Integer.parseInt(args[0]); // 1
+	        int b = Integer.parseInt(args[1]); // 6
+	        double random1, random2;
+
+	        if(a < b) {
+	            random1 = (b-a)*Math.random() + a;
+	            random2 = (b-a)*Math.random() + a;
+	        }
+	        else {
+	            random1 = (a-b)*Math.random() + b;
+	            random2 = (a-b)*Math.random() + b;
+	        }
+	        double sum = random1 + random2;
+	        System.out.println("Random Number" +(int)sum);
+
+	}
+
+}
