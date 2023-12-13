c24821676d987cae0c7401fde0b9d5bebff03c1e
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2023-12-14 00:07:29 +0530

    Added CommandLine - Q06

---
+++ Assignment02/cmd_06.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+public class cmd_06 {
+
+	public static void main(String[] args) {
+		char a =args[0].charAt(0);
+		int ch = a;
+		System.out.println("The ASCII value of " + a + " is " + ch);
+		
+	}
+
+}
