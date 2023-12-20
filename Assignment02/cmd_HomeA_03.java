5a3e7ddb82ca6b62389760f14e1ece22ba1cf917
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2023-12-21 00:02:32 +0530

    Updated changes in cmd_HomeA_03

--- Assignment02/cmd_HomeA_03.java
+++ Assignment02/cmd_HomeA_03.java
@@ -3,10 +3,10 @@ package assignment1;
 public class cmd_HomeA_03 {
 
 	public static void main(String[] args) {
-		int a = Integer.parseInt(args[0]);
-		int f = (a/1000);
-		int l = a%10;
-		int sum = f + 1;
+		int num = Integer.parseInt(args[0]);
+		int first = (num/1000);
+		int last = num%10;
+		int sum = first + last;
 		System.out.println("Sum: " + sum);
 	}
 
