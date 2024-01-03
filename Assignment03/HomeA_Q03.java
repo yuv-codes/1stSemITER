980dd73367f4aa8505f122eaadb4f4d381f9dae8
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2024-01-03 09:38:13 +0530

    Added HomeA_Q03

---
+++ Assignment03/HomeA_Q03.java
@@ -0,0 +1,22 @@
+public class HomeA_Q03 {
+    public static void main(String[] args) {
+        String month = null;
+        int random = (int)(Math.random()*12 + 1);
+
+        switch (random){
+            case 1 -> month = "January";
+            case 2 -> month = "February";
+            case 3 -> month = "March";
+            case 4 -> month = "April";
+            case 5 -> month = "May";
+            case 6 -> month = "June";
+            case 7 -> month = "July";
+            case 8 -> month = "August";
+            case 9 -> month = "September";
+            case 10 -> month = "October";
+            case 11 -> month = "November";
+            case 12 -> month = "December";
+        }
+        System.out.println("The generated number is " + random + " and the respective month is " + month);
+    }
+}
