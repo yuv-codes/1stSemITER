128f0e55fd02ef5fec640df3180da3daac359468
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2024-01-15 22:31:35 +0530

    Update Q05_HemisphereVolume.

--- Assignment02/Q05_HemisphereVolume.java
+++ Assignment02/Q05_HemisphereVolume.java
@@ -1,5 +1,12 @@
 package assignment1;
 
 public class Q05_HemisphereVolume {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of hemisphere: ");
+        double radius = sc.nextDouble();
+        double volume = 2.0/3 * Math.PI* Math.pow(radius,3);
+        System.out.println("The volume of hemisphere is: " + volume);
 
+  }
 }
