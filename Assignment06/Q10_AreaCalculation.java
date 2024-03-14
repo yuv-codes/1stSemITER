eb02f85cb03bde36ba24e198b41f37689bd094fa
Yuvraj Singh <146652142+yuvrajsingh2805@users.noreply.github.com>
2024-03-15 01:28:39 +0530

    Added A06-Q10

---
+++ Assignment06/Q10_AreaCalculation.java
@@ -0,0 +1,20 @@
+public class Q10_AreaCalculation {
+    public static double area(int b, int h){
+        return (0.5*b*h);
+    }
+    public static double area(double s){
+        return (s*s);
+    }
+    public static double area(int r){
+        return (Math.PI*r*r);
+    }
+    public static double area(double p, double b){
+        return (p*b);
+    }
+    public static void main(String[] args) {
+        System.out.println("Area of triangle is: " + area(5,10));
+        System.out.println("Area of square is: " + area(5.1));
+        System.out.println("Area of circle is: " + area(4));
+        System.out.println("Area of rectangle is: " + area(1.5,5.8));
+    }
+}
